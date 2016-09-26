2251b3972b02cdcef692d046f7e7d80f9df0ec14
kristofferek <inzeer@gmail.com>
2016-09-26 20:40:12 +0200

    Event.java now passes test

--- app/src/main/java/dat255/refugeeevent/model/Event.java
+++ app/src/main/java/dat255/refugeeevent/model/Event.java
@@ -5,4 +5,41 @@ package dat255.refugeeevent.model;
  */
 
 public class Event {
+    private String title, place, date, time;
+    private int nbrAttending;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getNbrAttending() {
+        return nbrAttending;
+    }
+    public void setNbrAttending(int nbrAttending) {
+        this.nbrAttending = nbrAttending;
+    }
 }
