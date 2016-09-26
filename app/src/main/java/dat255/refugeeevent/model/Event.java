c31dc5f773fda294a0346351707cdb301932232f
kristofferek <inzeer@gmail.com>
2016-09-26 20:37:32 +0200

    Added the Event class and made test for it

---
+++ app/src/main/java/dat255/refugeeevent/model/Event.java
@@ -0,0 +1,8 @@
+package dat255.refugeeevent.model;
+
+/**
+ * Created by Kristoffer on 2016-09-26.
+ */
+
+public class Event {
+}
---
+++ app/src/test/java/dat255/refugeeevent/EventTest.java
@@ -0,0 +1,29 @@
+package dat255.refugeeevent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import dat255.refugeeevent.model.Event;
+
+/**
+ * Created by Kristoffer on 2016-09-26.
+ */
+
+public class EventTest {
+    @Test
+    public void eventClassTest() throws Exception{
+        Event event = new Event();
+        event.setTitle("Title");
+        event.setPlace("Place");
+        event.setDate("2016/10/20");
+        event.setTime("10:00");
+        event.setNbrAttending(13);
+
+        assertEquals("Title", event.getTitle());
+        assertEquals("Place", event.getPlace());
+        assertEquals("2016/10/20", event.getDate());
+        assertEquals("10:00", event.getTime());
+        assertEquals(13, event.getNbrAttending());
+
+    }
+}
