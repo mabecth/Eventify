c6a7ed6cf347d9627dc026b854664a4e4820f187
kristofferek <inzeer@gmail.com>
2016-10-18 15:33:16 +0200

    Designed Settings view

--- app/src/main/java/dat255/eventify/activity/SettingsActivity.java
+++ app/src/main/java/dat255/eventify/activity/SettingsActivity.java
@@ -1,13 +1,9 @@
 package dat255.eventify.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Switch;
 
 import dat255.eventify.R;
 
@@ -20,15 +16,22 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initView();
 
 
         /*Spinner s = (Spinner) findViewById(R.id.firstdayspinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
         s.setAdapter(adapter);*/
+    }
+
+    public void initView(){
+        Switch notifications = (Switch) findViewById(R.id.notiSwitch);
+        
+
     }
 
 }
