50178c4c82bed5ff0e9ee6af9d516f423c843051
riwdo <racso.hansson@gmail.com>
2016-10-20 14:10:43 +0200

    added listener

---
+++ app/src/main/java/dat255/eventify/util/MyActivityListener.java
@@ -0,0 +1,14 @@
+package dat255.eventify.util;
+
+import android.content.Context;
+
+/**
+ * Created by Oscar on 2016-10-20.
+ */
+
+public interface MyActivityListener {
+
+    public void update();
+
+    public void displayAddress(String mAddressOutput);
+}
