0334ff454af8ea5104022bcc213712ea942a3083
Alex Sundbäck <sundbackalex@gmail.com>
2016-10-21 16:28:30 +0200

    Added test for Storing Events in StorageManager

---
+++ app/src/androidTest/java/dat255/eventify/StoreEventTest.java
@@ -0,0 +1,47 @@
+package dat255.eventify;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dat255.eventify.manager.StorageManager;
+import dat255.eventify.model.Event;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by alexsundback on 2016-10-21.
+ */
+
+public class StoreEventTest {
+
+    private List<Event> myTestEvents = new ArrayList<>();
+
+    Event event;
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        Assert.assertEquals("dat255.eventify", appContext.getPackageName());
+
+        StorageManager.getInstance().setContext(appContext);
+
+        event = new Event();
+        event.setId("1");
+
+        myTestEvents.add(event);
+
+        StorageManager.getInstance().storeEvents(myTestEvents);
+
+        assertEquals("1", StorageManager.getInstance().getEvents().get(0).getId());
+
+
+    }
+}
