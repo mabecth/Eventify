e637125813636ff2e2cb3008f40d3e31527bf958
Johannes <karlmattsson96@gmail.com>
2016-10-28 15:07:37 +0200

    changed packade from Mock to mock.

--- app/src/test/java/dat255/eventify/Mock/MockedContext.java
+++ app/src/test/java/dat255/eventify/Mock/MockedContext.java
@@ -4,10 +4,6 @@ package dat255.eventify.mock;
 import android.content.SharedPreferences;
 import android.test.mock.MockContext;
 
-/**
- * Created by Johannes on 2016-10-28.
- */
-
 public class MockedContext extends MockContext {
 
     public SharedPreferences getSharedPreferences(String name, int mode){
