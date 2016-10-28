045bd724a302238b0a623ead49c09e9e40412e8b
Alex Sundbäck <sundbackalex@gmail.com>
2016-10-28 12:10:24 +0200

    Added more test coverage

---
+++ app/src/test/java/dat255/eventify/eventutil/FavoriteTest.java
@@ -0,0 +1,89 @@
+package dat255.eventify.eventutil;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dat255.eventify.Mock.MockedContext;
+import dat255.eventify.eventutil.MyEventsManger;
+import dat255.eventify.manager.StorageManager;
+import dat255.eventify.model.Event;
+
+import static junit.framework.Assert.assertEquals;
+
+public class FavoriteTest {
+
+    /*
+    Test that tests the addToFavorite logic.
+     */
+
+    private Event event1;
+    private Event event2;
+    private MyEventsManger myEventsManger;
+    private StorageManager storageManager;
+    private List<Event> eventList;
+
+    @Before
+    public void setUp() {
+        storageManager = StorageManager.getInstance();
+        storageManager.setContext(new MockedContext());
+
+
+        event1 = new Event();
+        event1.setId("1");
+        event1.setTitle("Event Favorite");
+        event1.setDate("2020-12-12");
+        event1.setPlace("Place");
+        event1.setTime("10:00");
+        event1.setNbrAttending("13");
+        event1.setOwner("Organisation");
+        event1.setId("123456");
+        event1.setDesc("Description");
+        event1.setCover("url..");
+        event1.setDistance("14");
+        event1.setLatitude("12.3145");
+        event1.setLongitude("42.55");
+        eventList = new ArrayList<>();
+        eventList.add(event1);
+        storageManager.storeFavorites(eventList);
+
+        myEventsManger = MyEventsManger.getInstance();
+    }
+
+    @Test
+    public void useAppContext() throws Exception {
+
+        // Context of the app under test.
+
+
+        myEventsManger.setChosenEvent(event1);
+
+
+        myEventsManger.modifyFavorites();
+
+        assertEquals(false, myEventsManger.isFavorited());
+
+        event2 = new Event();
+        event2.setId("2");
+        event2.setTitle("Event Favorite 2");
+        event2.setDate("2019-12-12");
+        event2.setPlace("Place");
+        event2.setTime("10:00");
+        event2.setNbrAttending("13");
+        event2.setOwner("Organisation");
+        event2.setDesc("Description");
+        event2.setCover("url..");
+        event2.setDistance("14");
+        event2.setLatitude("12.3145");
+        event2.setLongitude("44.23");
+
+        eventList.add(event2);
+        storageManager.storeFavorites(eventList);
+
+        myEventsManger.setChosenEvent(event2);
+
+        assertEquals("2", myEventsManger.getFavorites().get(0).getId());
+    }
+}
--- app/src/test/java/dat255/eventify/helper/SortByDateTest.java
+++ app/src/test/java/dat255/eventify/helper/SortByDateTest.java
@@ -1,73 +1,86 @@
 package dat255.eventify.helper;
 
-import android.util.Log;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 import dat255.eventify.model.Event;
-import dat255.eventify.helper.SortByDate;
+
 
 import static junit.framework.Assert.assertEquals;
 
+
 public class SortByDateTest {
 
-    Event event1;
-    Event event2;
-    Event event3;
-    Event event4;
-    Event event5;
-    Event event6;
-    Event event7;
 
-    List<Event> fullList;
+    private static Event event1;
+    private static Event event2;
+    private static Event event3;
+    private static Event event4;
+    private static Event event5;
+    private static Event event6;
+    private static Event event7;
 
-    public SortByDateTest() throws Exception {
-        sortByDateTest1();
-        sortByDateTest2();
-    }
 
-    public void initList() {
+    private static List<Event> fullList;
+
+    /*
+    Initializing variables for testing..
+     */
+    @BeforeClass
+    public static void initList() {
         fullList = new ArrayList<>();
+
 
         event1 = new Event();
         event1.setDate("2016-10-20");
         event1.setTime("10:00");
         fullList.add(event1);
 
+
         event2 = new Event();
         event2.setDate("2016-10-14");
         event2.setTime("13:00");
         fullList.add(event2);
+
 
         event3 = new Event();
         event3.setDate("2016-10-14");
         event3.setTime("11:30");
         fullList.add(event3);
 
+
         event4 = new Event();
         event4.setDate("2016-10-14");
         event4.setTime("11:29");
         fullList.add(event4);
+
 
         event5 = new Event();
         event5.setDate("2015-10-14");
         event5.setTime("11:28");
         fullList.add(event5);
 
+
         event6 = new Event();
         event6.setDate("1996-04-06");
         event6.setTime("17:36");
         fullList.add(event6);
 
+
     }
+
 
     @Test
     public void sortByDateTest1() throws Exception {
         initList();
         SortByDate.sortDates(fullList);
+
 
         assertEquals("1996-04-06 17:36", fullList.get(0).getDate() + " " + fullList.get(0).getTime());
         assertEquals("2015-10-14 11:28", fullList.get(1).getDate() + " " + fullList.get(1).getTime());
@@ -77,34 +90,46 @@ public class SortByDateTest {
         assertEquals("2016-10-20 10:00", fullList.get(5).getDate() + " " + fullList.get(5).getTime());
     }
 
+
     @Test
     public void sortByDateTest2() throws Exception {
         initList();
 
+
         List<Event> list1 = new ArrayList<>();
+
 
         list1.add(event2);
         list1.add(event6);
 
+
         SortByDate.sortDates(list1);
+
 
         assertEquals("1996-04-06 17:36", list1.get(0).getDate() + " " + list1.get(0).getTime());
         assertEquals("2016-10-14 13:00", list1.get(1).getDate() + " " + list1.get(1).getTime());
     }
 
+
     @Test
     public void sortByDateTest3() throws Exception {
         initList();
 
+
         List<Event> list2 = new ArrayList<>();
+
 
         list2.add(event3);
         list2.add(event4);
 
+
         SortByDate.sortDates(list2);
+
 
         assertEquals("2016-10-14 11:29", list2.get(0).getDate() + " " + list2.get(0).getTime());
         assertEquals("2016-10-14 11:30", list2.get(1).getDate() + " " + list2.get(1).getTime());
     }
 
+
 }
+
